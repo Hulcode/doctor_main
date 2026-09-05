4e5279ebff404e09265f00a8a18e2de05d373d48
Hulcode <alhassanahmedsoliman@gmail.com>
2026-09-05 22:20:44 +0300

    iphone 6 problem

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,6 @@
-import oklabFunction from "@csstools/postcss-oklab-function";
-import colorMixFunction from "@csstools/postcss-color-mix-function";
-
 const config = {
   plugins: {
     tailwindcss: {},
-    "@csstools/postcss-oklab-function": { preserve: true },
-    "@csstools/postcss-color-mix-function": { preserve: true },
     autoprefixer: {},
   },
 };
